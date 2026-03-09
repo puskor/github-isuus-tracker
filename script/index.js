ab522947ff852517568c84dc96a9c7cf93689ebf
Puskor Roy <puskarkumarroy329@gmail.com>
2026-03-09 14:00:12 +0600

    modal work done

--- script/index.js
+++ script/index.js
@@ -1,7 +1,35 @@
-// fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
-//     .then((res) => res.json())
-//     .then((data) => console.log(data.data));
 
+
+
+
+const load_modal = async (id) => {
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
+    const res = await fetch(url);
+    const details = await res.json();
+    console.log(details.data);
+    show_modal(details.data);
+}
+
+
+
+const show_modal = (item)=>{
+    
+    const title = document.getElementById("title");
+    const opener = document.getElementById("opener");
+    const modal_date = document.getElementById("modal_date");
+    const modal_description = document.getElementById("modal_description");
+    const Assignee_name = document.getElementById("Assignee_name");
+    const Priority = document.getElementById("Priority");
+    const status = document.getElementById("status");
+    title.innerText= `${item.title}`;
+    opener.innerText = `${item.assignee}`
+    modal_date.innerText =`${item.createdAt}`
+    modal_description.innerText=`${item.description}`
+    Assignee_name.innerHTML = `${item.assignee}`
+    Priority.innerHTML = `${item.priority}`
+    status.innerHTML =`${item.status}`
+    my_modal_5.showModal();
+}
 
 
 const stor = document.getElementById("AllCard");
@@ -26,7 +54,7 @@ function showCard(data) {
         }
         const div = document.createElement("div");
         div.innerHTML = `
-                            <div class="p-5 card bg-base-100 shadow-lg  border-t-8 border-[${topColor}]">
+                    <div onClick="load_modal(${element.id})" class="p-5 card bg-base-100 shadow-lg  border-t-8 border-[${topColor}]">
                     <div class=" flex justify-between">
                         <img class="w-8 h-8" src="${statusImg}" alt="">
                         <h1 class="bg-[${bgColor}] text-[${textColor}] px-3 py-1 rounded-2xl">${element.priority.toUpperCase()}</h1>
@@ -84,13 +112,21 @@ document.getElementById("main_btn").addEventListener("click", (even) => {
 
 const data = document.getElementById("total_issus");
 
+const loading = document.getElementById("loading")
 fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
-        .then((res) => res.json())
-        .then((data) => showCard(data.data));
+    .then((res) => res.json())
+    .then((data) => {
+        loading.classList.remove("hidden");
+        showCard(data.data)
+        loading.classList.add("hidden");
+    });
+
 
 
 function select_btn(type) {
-    stor.innerText=" ";
+    stor.innerText = " ";
+    loading.classList.remove("hidden");
+
     fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
         .then((res) => res.json())
         .then((data) => {
@@ -102,12 +138,13 @@ function select_btn(type) {
             }
             else if (type == "closed") {
                 filterData = allData.filter((item) => item.status == "closed");
-            } 
+            }
             else {
                 filterData = allData;
             }
             showCard(filterData);
+            loading.classList.add("hidden");
         })
-
-
 }
+
+
